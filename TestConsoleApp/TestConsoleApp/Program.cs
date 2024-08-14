4c34a67ed9edc697008b4d6e0278b391af3b5221
Brandon Smyth <itsapolloofficial@gmail.com>
2024-08-14 20:27:38 +0200

    Updated Program.cs
    Added bad syntax!

--- TestConsoleApp/TestConsoleApp/Program.cs
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -11,6 +11,7 @@ namespace TestConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+           console.WriteLine("Hello World! But with bad syntax and indenation")
         }
     }
 }
